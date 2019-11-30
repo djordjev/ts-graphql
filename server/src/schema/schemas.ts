1473d32912533c66b70843cbdac79144f08a260b
Djordje Vukovic <djvukovic@gmail.com>
2019-11-30 11:34:38 +0100

    eslint server

--- server/src/schema/schemas.ts
+++ server/src/schema/schemas.ts
@@ -7,13 +7,6 @@ import rootSchema from '../api/root/schema';
 import meSchema from '../api/user/me/schema';
 import userSchema from '../api/user/schema';
 
-const schemas: DocumentNode[] = [
-  userSchema,
-  rootSchema,
-  signupSchema,
-  meSchema,
-  loginSchema,
-  logoutSchema
-];
+const schemas: DocumentNode[] = [userSchema, rootSchema, signupSchema, meSchema, loginSchema, logoutSchema];
 
 export default schemas;
