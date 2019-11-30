c71c4297fa62f4f081a58cedeeef6c72b5f261f6
Djordje Vukovic <djvukovic@gmail.com>
2019-11-30 11:01:31 +0100

    stylelint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
     },
     rules: {
         "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/interface-name-prefix": "off"
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/no-inferrable-types": "off"
     },
     settings: {
         react: {
--- client/src/client/auth/pages/login/Login.tsx
+++ client/src/client/auth/pages/login/Login.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import * as styles from './styles.css';
 
 const Login: React.FC = () => {
-  const copy = 'Djordje Vukovicd';
+  const copy: string = 'Djordje Vukovicd';
   return <div className={styles.component}>{copy}</div>;
 };
 
